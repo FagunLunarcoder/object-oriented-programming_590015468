c0c312e5299766feb5740ac0c472b42afee7b8be
FagunLunarcoder <fagunwasan@gmail.com>
2026-02-05 15:59:23 +0530

    Add files via upload

---
+++ oopslab2.java/Q5.java
@@ -0,0 +1,11 @@
+public class Q5 {
+
+    int x = 55;  
+
+    public static void main(String[] args) {
+
+        Q5 obj = new Q5();   // object creation
+
+        System.out.println("Value of x = " + obj.x); 
+    }
+}
---
+++ oopslab2.java/Q6.java
@@ -0,0 +1,9 @@
+public class Q6 {
+    public static void main(String[] args) {
+         int sum = 0;
+          for (int i = 0; i < args.length; i++) {
+            sum = sum + Integer.valueOf(args[i]);
+        }
+         System.out.println("Sum of array elements = " + sum);
+    }
+}
